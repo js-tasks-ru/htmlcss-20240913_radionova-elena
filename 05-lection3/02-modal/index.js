0d3d5975a83e09c54bf1c02c98cb173b5edf082a
ElenaVlasenko <radionovaelena124@gmail.com>
2024-10-03 18:49:50 +0300

    modal`s added

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,7 +1,8 @@
-(function() {
+(function () {
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
+    const modalBackdrop = document.getElementsByClassName("modalBackdrop")[0];
 
     if (!close || !modalOpen || !modal) {
         return;
@@ -10,10 +11,12 @@
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
         document.body.overflow = 'hidden';
+        modalBackdrop.style.display = "block";
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
         document.body.overflow = 'initial';
+        modalBackdrop.style.display = "none";
     });
 })();
